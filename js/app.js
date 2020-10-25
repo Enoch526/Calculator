d378954abdd046be102156cfc6317d1b326cd73e
dhivyanj <67097553+dhivyanj@users.noreply.github.com>
2020-10-25 14:24:22 +0530

    File Upload go brrrrrrrr

---
+++ js/app.js
@@ -0,0 +1,34 @@
+
+// Javascript
+
+function add() {
+    var num1 = prompt('Enter the First Number to Add:')
+    var num2 = prompt('Enter the Second Number to Add:')
+    num1 = parseFloat(num1)
+    num2 = parseFloat(num2)
+    alert(num1 + num2)
+};
+
+function subtract() {
+    var num1 = prompt('Enter the First Number to Subtract:')
+    var num2 = prompt('Enter the Second Number to Subtract:')
+    num1 = parseFloat(num1)
+    num2 = parseFloat(num2)
+    alert(num1 - num2)
+};
+
+function multiply() {
+    var num1 = prompt('Enter the First Number to Multiply:')
+    var num2 = prompt('Enter the Second Number to Multiply:')
+    num1 = parseFloat(num1)
+    num2 = parseFloat(num2)
+    alert(num1 * num2)
+};
+
+function Divide() {
+    var num1 = prompt('Enter the First Number to Divide:')
+    var num2 = prompt('Enter the Second Number to Divide:')
+    num1 = parseFloat(num1)
+    num2 = parseFloat(num2)
+    alert(num1 / num2)
+};
